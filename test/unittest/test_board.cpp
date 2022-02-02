c082b31116f1bebf2436903382a369649e45a49b
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-02-02 19:34:48 +0100

    Refactor: replace magic numbers with function calls

--- test/unittest/test_board.cpp
+++ test/unittest/test_board.cpp
@@ -28,11 +28,11 @@ TEST_CASE("Board initially contains all options in all positions")
 {
     Board const board;
 
-    for (auto y = 0; y < 9; ++y)
+    for (auto y = 0; y < board.height(); ++y)
     {
-        for (auto x = 0; x < 9; ++x)
+        for (auto x = 0; x < board.width(); ++x)
         {
-            for (auto v = 1; v <= 9; ++v)
+            for (auto v : board.options())
             {
                 REQUIRE(board.has_option(x, y, v));
             }
