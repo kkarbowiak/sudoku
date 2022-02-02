87053f304be516e961fd11e5f98d0ee168e4b59d
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-02-02 22:01:07 +0100

    Refactor: move common code to separate function

--- test/unittest/test_board.cpp
+++ test/unittest/test_board.cpp
@@ -66,3 +66,65 @@ TEST_CASE("Board initially is not fixed anywhere")
         }
     }
 }
+
+TEST_CASE("Fixing a value at x,y removes it from x-column and y-row")
+{
+    for (auto y = 0; y < Board::height(); ++y)
+    {
+        for (auto x = 0; x < Board::width(); ++x)
+        {
+            Board board;
+            auto const v = Board::options().front();
+
+            board.fix_option(x, y, v);
+
+            for (auto yy = 0; yy < Board::height(); ++yy)
+            {
+                if (yy != y)
+                {
+                    REQUIRE(!board.has_option(x, yy, v));
+                }
+            }
+
+            for (auto xx = 0; xx < Board::width(); ++xx)
+            {
+                if (xx != x)
+                {
+                    REQUIRE(!board.has_option(xx, y, v));
+                }
+            }
+        }
+    }
+}
+
+auto round_coord(int c)
+{
+    return 3 * (c / 3);
+}
+
+TEST_CASE("Fixing a value at x,y removes it from corresponding 3x3 square")
+{
+    for (auto y = 0; y < Board::height(); ++y)
+    {
+        for (auto x = 0; x < Board::width(); ++x)
+        {
+            Board board;
+            auto const v = Board::options().front();
+
+            board.fix_option(x, y, v);
+
+            auto const y_start = round_coord(y);
+            auto const x_start = round_coord(x);
+            for (auto yy = y_start; yy < y_start + 3; ++yy)
+            {
+                for (auto xx = x_start; xx < x_start + 3; ++xx)
+                {
+                    if ((yy != y) || (xx != x))
+                    {
+                        REQUIRE(!board.has_option(xx, yy, v));
+                    }
+                }
+            }
+        }
+    }
+}
