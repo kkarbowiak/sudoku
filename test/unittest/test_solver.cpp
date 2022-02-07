73d411f9981b47272f537d3c3e5ccb7505d73fc1
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-02-07 22:23:16 +0100

    Improve comparison in unit tests

--- test/unittest/test_solver.cpp
+++ test/unittest/test_solver.cpp
@@ -22,8 +22,7 @@ TEST_CASE("Solver identifies a solved board and returns it as a solution")
 
     auto solutions = solver.get_solutions();
 
-    REQUIRE(solutions.size() == 1u);
-    REQUIRE(solutions.front() == Solution{{{
+    REQUIRE(solutions == std::vector<Solution>{{{{
         {{5u, 3u, 4u, 6u, 7u, 8u, 9u, 1u, 2u}},
         {{6u, 7u, 2u, 1u, 9u, 5u, 3u, 4u, 8u}},
         {{1u, 9u, 8u, 3u, 4u, 2u, 5u, 6u, 7u}},
@@ -32,7 +31,7 @@ TEST_CASE("Solver identifies a solved board and returns it as a solution")
         {{7u, 1u, 3u, 9u, 2u, 4u, 8u, 5u, 6u}},
         {{9u, 6u, 1u, 5u, 3u, 7u, 2u, 8u, 4u}},
         {{2u, 8u, 7u, 4u, 1u, 9u, 6u, 3u, 5u}},
-        {{3u, 4u, 5u, 2u, 8u, 6u, 1u, 7u, 9u}}}}});
+        {{3u, 4u, 5u, 2u, 8u, 6u, 1u, 7u, 9u}}}}}});
 }
 
 TEST_CASE("Solver finds and returns a single solution")
@@ -53,8 +52,7 @@ TEST_CASE("Solver finds and returns a single solution")
 
     auto solutions = solver.get_solutions();
 
-    REQUIRE(solutions.size() == 1u);
-    REQUIRE(solutions.front() == Solution{{{
+    REQUIRE(solutions == std::vector<Solution>{{{{
         {{5u, 3u, 4u, 6u, 7u, 8u, 9u, 1u, 2u}},
         {{6u, 7u, 2u, 1u, 9u, 5u, 3u, 4u, 8u}},
         {{1u, 9u, 8u, 3u, 4u, 2u, 5u, 6u, 7u}},
@@ -63,5 +61,5 @@ TEST_CASE("Solver finds and returns a single solution")
         {{7u, 1u, 3u, 9u, 2u, 4u, 8u, 5u, 6u}},
         {{9u, 6u, 1u, 5u, 3u, 7u, 2u, 8u, 4u}},
         {{2u, 8u, 7u, 4u, 1u, 9u, 6u, 3u, 5u}},
-        {{3u, 4u, 5u, 2u, 8u, 6u, 1u, 7u, 9u}}}}});
+        {{3u, 4u, 5u, 2u, 8u, 6u, 1u, 7u, 9u}}}}}});
 }
