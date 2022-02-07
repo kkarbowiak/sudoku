a9a8c42a44ba3d8835c5e508e036f1e198987bb7
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-02-07 11:24:59 +0100

    Add assertions in BoardReader

--- board_reader.h
+++ board_reader.h
@@ -6,6 +6,9 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <cctype>
+#include <cassert>
 
 
 class BoardReader
@@ -16,6 +19,10 @@ class BoardReader
 
 inline auto BoardReader::read_board(std::vector<std::string> const & values) const
 {
+    assert(values.size() <= Board::height());
+    assert(std::all_of(values.begin(), values.end(), [](auto const & row) { return row.size() <= Board::width(); }));
+    assert(std::all_of(values.begin(), values.end(), [](auto const & row) { return std::all_of(row.begin(), row.end(), [](unsigned char c) { return std::isdigit(c); }); }));
+
     Board board;
 
     auto y = 0u;
